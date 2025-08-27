401f40384030c5f01825635b8fd42b281553a566
Filip <wojtasik.f03@htlwienwest.at>
2025-08-27 17:28:40 +0200

    (conf): add Prisma schema and centralized Prisma Client singleton

---
+++ server/src/utils/prismaClient.js
@@ -0,0 +1,12 @@
+// ! Centralized Prisma Client singleton to avoid creating multiple connections
+const { PrismaClient } = require('@prisma/client');
+
+let prisma;
+
+if (!global.__prisma) {
+  global.__prisma = new PrismaClient();
+}
+
+prisma = global.__prisma;
+
+module.exports = prisma;
