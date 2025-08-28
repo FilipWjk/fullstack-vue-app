016df072ef581bae0e1302746610da2ee977d2c6
Filip <wojtasik.f03@htlwienwest.at>
2025-08-28 21:32:15 +0200

    (feat): integrate Toast notification library and initialize authentication store in main application

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,33 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth.ts'
+
+import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
+app.use(Toast, {
+  position: 'top-right',
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+})
+
+const authStore = useAuthStore()
+authStore.init()
 
 app.mount('#app')
